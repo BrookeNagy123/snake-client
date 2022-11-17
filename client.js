2bc5c969efe0d30e039ae71d6138a7174ac3ca43
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-16 17:35:58 -0700

    Add Snake's Initials

--- client.js
+++ client.js
@@ -13,6 +13,14 @@ const connect = function () {
     console.log(data)
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: BEN");
+  });
+
   return conn;
 
 };
