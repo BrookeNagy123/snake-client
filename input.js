c1d000fb2ecb0ed243654336e173f074d7434cc4
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-18 13:36:06 -0700

    WASD Movement Input Added for Snake

--- input.js
+++ input.js
@@ -1,20 +1,31 @@
 // setup interface to handle user input from stdin
+let connection;
 
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  const handleUserInput = stdin.on("data",(key) => {
+    if (key === '\u0003') {
+      process.exit();
+    } 
+    if(key === "w"){
+      conn.write("Move: up");
+    } 
+    if(key === "a"){
+      conn.write("Move: left");
+    } 
+    if(key === "s"){
+      conn.write("Move: down")
+    } 
+    if(key === "d"){
+      conn.write("Move: right");
+    }
+  });
   return stdin;
 };
 
-const handleUserInput = function(){
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-}
 
 module.exports = {setupInput};
