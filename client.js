23d5b171cf161a3ad25c7684ec81ecf8272f034d
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-18 14:25:47 -0700

    Sayings added and moved out PORT and IP into constants file

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "192.168.0.11",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });;
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
   conn.on("data", (data) => {
     console.log(data)
   });
@@ -25,9 +26,6 @@ const connect = function () {
   //   setInterval(() => {
   //     conn.write("Move: up");}, 5000);
   // });
-
-
-
   return conn;
 
 };
