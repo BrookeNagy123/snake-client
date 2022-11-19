3db61a215bf33b14719b5913f2fcf51a2d86fa34
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-18 17:01:38 -0700

    Cleaned up comments in the code

--- client.js
+++ client.js
@@ -1,19 +1,19 @@
 const net = require("net");
 const { IP, PORT } = require("./constants");
 
-// Establishes a connection with the game server
+// Establishes a connection with the game server.
 const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
   });
 
-  // Interpret incoming data as text
+  // Interpret incoming data as text.
   conn.setEncoding("utf8");
   conn.on("data", (data) => {
     console.log(data);
   });
-
+  //Display the connection was successful. 
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
   });
