c13ee1461a750647c8308d7f3a73b1568ea1118d
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-18 17:02:23 -0700

    Added comments to the code

--- play.js
+++ play.js
@@ -1,3 +1,5 @@
+//Play.js is used to play the game! Requires files from Input and Client. 
+
 const {connect} = require("./client");
 const {setupInput} = require("./input");
 console.log("Connecting ...");
