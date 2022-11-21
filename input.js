0f8f97bff63efbe8c637a67ebef6e1f1bb3d2142
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-21 16:46:19 -0700

    Remove the handleUserInput function out of setupInput function

--- input.js
+++ input.js
@@ -7,35 +7,36 @@ const setupInput = function(conn) {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = stdin.on("data",(key) => {
-    // Below are the keys to direct the snake and a few different options to say something in the game.
-    if (key === '\u0003') {
-      process.exit();
-    }
-    if (key === "w") {
-      conn.write("Move: up");
-    }
-    if (key === "a") {
-      conn.write("Move: left");
-    }
-    if (key === "s") {
-      conn.write("Move: down");
-    }
-    if (key === "d") {
-      conn.write("Move: right");
-    }
-    if (key === "b") {
-      conn.write("Say: Boo");
-    }
-    if (key === "n") {
-      conn.write("Say: Sss");
-    }
-    if (key === "m") {
-      conn.write("Say: Zoom");
-    }
-  });
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
+const handleUserInput = (key) => {
+  // Below are the keys to direct the snake and a few different options to say something in the game.
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "b") {
+    connection.write("Say: Boo");
+  }
+  if (key === "n") {
+    connection.write("Say: Sss");
+  }
+  if (key === "m") {
+    connection.write("Say: Zoom");
+  }
+};
 
 module.exports = {setupInput};
